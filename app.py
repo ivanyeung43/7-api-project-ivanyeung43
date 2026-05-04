23c9a81d5f7ed416b2e325f7cf8637fe80118f1a
ivanyeung43 <ivany43@nycstudents.net>
2026-05-04 19:40:21 -0400

    Update app.py

--- app.py
+++ app.py
@@ -13,14 +13,57 @@ def getWeather(lat, lon, APIkey):
         return None
 
     data = response.json()
-    weatherDATA = {
+    
+    weather =  {
         "main_temp": [data["main"]["temp"]],
         "max_temp": [data["main"]["temp_max"]],
         "min_temp": [data["main"]["temp_min"]],
         "wind_speed": [data["wind"]["speed"]],
+
+        "clouds": [data["clouds"]["all"]]
     }
-    return weatherDATA
-getWeather(input("Enter latitude"), input("Enter Longitude"), "077be3b7d8efd9180e8de0642a4ce332")
+    return weather
+
+weatherDATA = getWeather(input("Enter latitude"), input("Enter Longitude"), "077be3b7d8efd9180e8de0642a4ce332")
+endPROGRAM = ""
+while endPROGRAM != 10:
+    questionDICT = {
+        "1": "Average Temperature",
+        "2": "Max Temperature",
+        "3": "Min Temperature",
+        "4": "Wind speed",
+        "5": "Rain",
+        "6": "Cloud Percentage"
+    }
+    for key, value in questionDICT.items():
+        print(key, "→", value)
+    userSELECTION = (input("Select information. Type 10 to end the program"))
+    farenMAIN = int(weatherDATA["main_temp"][-1])
+    farenMAX = int(weatherDATA["max_temp"][-1])
+    farenMIN = int(weatherDATA["min_temp"][-1])
+    if int(userSELECTION) == 10:
+        break
+    elif int(userSELECTION) == 1:
+        print(round(farenMAIN - 273.15) * 9/5 + 32)
+    elif int(userSELECTION) == 2:
+        farenMAX = int(weatherDATA["max_temp"][-1])
+        if farenMAX == farenMAIN:
+            print("MAX and MIN temperatures are only available for large urban areas")
+        else:
+            print(round(farenMAX - 273.15) * 9/5 + 32)
+    elif int(userSELECTION) == 3:
+        if farenMIN == farenMAX:
+            print("MAX and MIN temperatures are only available for large urban areas")
+        else:
+            print(round(farenMIN - 273.15) * 9/5 + 32)
+    elif int(userSELECTION) == 4:
+        print(weatherDATA["wind_speed"], "km")
+    elif int(userSELECTION) == 5:
+        print(weatherDATA["rain"])
+    elif int(userSELECTION) == 6:
+        print(weatherDATA["clouds"], "%")
+print("WeatherAPP has ended")
+
 
 
 
